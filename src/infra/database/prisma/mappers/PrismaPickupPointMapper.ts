6335f55d76ab2f2659209612d105339c5fe8341e
Pedro Lara <pedroalveslara@gmail.com>
2024-11-13 15:08:11 -0300

    Update PrismaPickupPointMapper.ts

--- src/infra/database/prisma/mappers/PrismaPickupPointMapper.ts
+++ src/infra/database/prisma/mappers/PrismaPickupPointMapper.ts
@@ -1,5 +1,6 @@
 import { PickupPoint } from "../../../../modules/pickup_point/entities/pickup_point.entity";
-import {Pickup_Point as PickupPointRaw} from "@prisma/client"
+import { Pickup_Point as PickupPointRaw } from '@prisma/client';
+
 
 export class PrismaPickupPointMapper {
 
