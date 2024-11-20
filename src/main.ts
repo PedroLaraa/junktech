f5977078bcef79ad21a5ef7391baa019b7b3594b
Pedro Lara <pedroalveslara@gmail.com>
2024-11-20 16:58:53 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Requested",
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+  })
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(process.env.PORT ?? 3000);
 }
